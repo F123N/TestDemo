6d1fc480161608d36d752713239a0529940f516f
chennenggh <1148798538@qq.com>
2016-10-22 12:09:03 +0800

    增加一个类

---
+++ testDemo/testDemo/YidaHuLian.h
@@ -0,0 +1,13 @@
+//
+//  YidaHuLian.h
+//  testDemo
+//
+//  Created by mac2015 on 16/10/22.
+//  Copyright © 2016年 gao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YidaHuLian : NSObject
+
+@end
---
+++ testDemo/testDemo/YidaHuLian.m
@@ -0,0 +1,13 @@
+//
+//  YidaHuLian.m
+//  testDemo
+//
+//  Created by mac2015 on 16/10/22.
+//  Copyright © 2016年 gao. All rights reserved.
+//
+
+#import "YidaHuLian.h"
+
+@implementation YidaHuLian
+
+@end
