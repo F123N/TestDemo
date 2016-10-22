3bd833694cd7c8d63e4494b9615794266dcf3acc
chennenggh <1148798538@qq.com>
2016-10-22 12:42:42 +0800

    新建一个工程

---
+++ testDemo2/testDemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testDemo2
+//
+//  Created by mac2015 on 16/10/22.
+//  Copyright © 2016年 gao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
